7594e3c1162b43bcdd561c921598be36d10ead12
bowch51 <bowch51@users.noreply.github.com>
2018-09-15 22:45:44 +0300

    Third Commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>reCaptcha test</title>
+	<script src='https://www.google.com/recaptcha/api.js'></script>
+</head>
+<body>
+		<form action="index.php">
+		<a href="test.php">Переход</a>
+	</form>
+</body>
+</html>
