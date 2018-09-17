4a079029e55db3c529945710be1dbf3aac5e49d7
bowch51 <bowch51@users.noreply.github.com>
2018-09-17 19:39:09 +0300

    MegaUp9

---
+++ test3/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>bowch51 </dead></title>
+	<meta name="viewport" content="initial-scale = 1.0, maximum-scale = 1.0, user-scalable = no, width = device-width" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.."></script>
+	<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.9"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+<link href="https://fonts.googleapis.com/css?family=Gaegu|Knewave" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Bungee+Shade" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Graduate" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Anton|Fjalla+One" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Anton|Fjalla+One|Kanit" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<p class = "name-rap"><span class="typed"></span></p>
+		<p class = "name-rap"><span class="typed1"></span></p>
+	</header>
+<div id="hellopreloader"><div id="hellopreloader_preload"></div>
+<div id="trailer" class="is_overlay">
+<video muted autoplay autoplay="autoplay" id="video" width="100%" height="auto" autoplay="autoplay" >
+<source autoplay="autoplay" src="video.mp4"></source>
+</video>
+	<div id="mask">
+<div class="Center-html">
+</div>
+<div id = "main">
+	<embed id="music" src="music.mp3" autostart="true" hidden="true" loop="false"> </embed>
+	<div id = "NickName">
+	<h2>BOWCH51</h2>
+	<div id = "Social">
+	<a class="Social" href="https://vk.com/vevshin0">VK </a>|
+	<a class="Social" href="https://www.t.me/lesone">TELEGRAM</a>
+
+	</div>
+	<div class = "how1">
+	<p><span class="how">PRESS F TO PAY *RESPECT*</span></p>
+</div>
+	</div>
+
+</div>
+</div>
+
+</div>
+
+<!--<script src="js/script.js"></script>
+<script type="text/javascript">
+var typed = new Typed(".typed", {
+// Waits 1000ms after typing "First"
+strings: ["Copyright 2018 (C) "], // Слова,которые будут писаться.
+typeSpeed:180, //Скорость написания.
+backSpeed:180, // Скорость стирания.
+loop:true //Создаёт цикл бесконечного повторения.
+});
+</script>
+<script type="text/javascript">
+var typed = new Typed(".typed1", {
+// Waits 1000ms after typing "First"
+strings: ["Copy design is prosecuted by law"], // Слова,которые будут писаться.
+typeSpeed:180, //Скорость написания.
+backSpeed:180, // Скорость стирания.
+loop:true //Создаёт цикл бесконечного повторения.
+});
+</script>
+-->
+</body>
+</html>
